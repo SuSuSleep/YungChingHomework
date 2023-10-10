8bb994b539f2d63d4adb32d51589973111b3e9c5
Johnson Huang <joh449449@gmail.com>
2023-10-10 22:09:21 +0800

    修改為複數資料夾名稱
    - ./Repository => ./Repositories

---
+++ Repositories/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using YungChingHomework.DBModels;
+using YungChingHomework.DTOs.Infos;
+
+namespace YungChingHomework.Repository
+{
+    public interface ICustomerRepository
+    {
+        public IEnumerable<Customer> GetList();
+        public Customer Get(int Id);
+        public long Create(NewCustomerInfo NewCustomer);
+        public bool Update(CustomerInfo Customer);
+        public bool Delete(long Id);
+    }
+}
---
+++ Repositories/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using YungChingHomework.DBModels;
+using YungChingHomework.DTOs.Infos;
+
+namespace YungChingHomework.Repository
+{
+    public interface IOrderDetailRepository
+    {
+        public IEnumerable<OrderDetail> GetList();
+        public OrderDetail Get(long Id);
+        public long Create(NewOrderDetailInfo NewOrderDetailInfo);
+        public bool Update(OrderDetailInfo OrderDetailInfo);
+        public bool Delete(long Id);
+    }
+}
